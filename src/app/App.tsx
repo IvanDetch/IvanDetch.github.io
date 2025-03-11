e61e9501545c8a7ab6e16d9a5167f17c8d1db222
Ivan Komrakov <komrakov@alternativa.games>
2025-03-11 23:07:42 +0300

    feat: add personal information to App.tsx feat: modify style App.css feat: modify package.json feat: modify webpack.config.json

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <div className='App-body'>
+          <h1>Иван Комраков</h1>
+          <p>
+            <strong>Цель: </strong>
+            <span>Хочу приобрести/структурировать новые знания и навыки о React, чтобы применить их на работе.</span>
+          </p>
+          <p>
+            <strong>Технологии: </strong>
+            <ul>
+              <li>JavaScript (ES6), jQuery, HTML, CSS3 (препроцессоры SCSS/SASS), Bootstrap, PHP 7.3+, Laravel 8+ .</li>
+              <li>Частичная работа с React 18, Node.js.</li>
+              <li>Rest API.</li>
+              <li>Сборщики: Laravel mix. React vite/WebPack .</li>
+              <li>Composer, NPM, Kubernets, Docker.</li>
+              <li>SQL(MySQL,MariaDB), MongoDB.</li>
+              <li>Git, GitLab, GitLab CI/CD.</li>
+              <li>CMS WordPress.</li>
+              <li>Figma, Jira.</li>
+            </ul>
+          </p>
+          <p>
+            <strong>Опыт: </strong>
+            <span>Имею опыт работы в области веб-разработки (профессиональный опыт 7 лет), обладаю знаниями и навыками верстки, Frontend и Backend-технологий.</span>
+          </p>
+          <p>
+          <strong>Контакты: </strong>
+          <span>kmrakvi@gmail.com</span>
+          </p>
+        </div>
       </header>
     </div>
   );
